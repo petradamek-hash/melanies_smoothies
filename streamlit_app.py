ad2240d9767adcbd87e3f128acca65f0371e5e39
petradamek-hash <petr.adamek@sophias.cz>
2026-03-18 14:50:10 +0100

    32

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,8 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        api_fruit_name = fruit_chosen.lower().strip()
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + api_fruit_name)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string) 
