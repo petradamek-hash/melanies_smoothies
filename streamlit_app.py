f635a61e67c4d2d087a9f92bcebd9d0e1ac6f3ee
petradamek-hash <petr.adamek@sophias.cz>
2026-03-18 14:30:04 +0100

    řádek 50

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,5 @@ if ingredients_list:
 #New section to display smoothiefroot nutrition information
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(date=smoothiefroot_response.json(), use_container_width=True)
